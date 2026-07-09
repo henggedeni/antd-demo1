b310f8c6e84edf858c6affc9039092c3af65ca78
wangyue <1198810568@qq.com>
2026-07-09 14:01:29 +0800

    feat: 示例

--- src/components/Guide/Guide.tsx
+++ src/components/Guide/Guide.tsx
@@ -1,14 +1,21 @@
 import { Layout, Row, Typography } from 'antd';
-import React from 'react';
+import { forwardRef, useImperativeHandle } from 'react';
 import styles from './Guide.less';
 
-interface Props {
-  name: string;
+interface PropsType {
+  name?: string;
 }
 
 // 脚手架示例组件
-const Guide: React.FC<Props> = (props) => {
+const Guide = forwardRef((props: PropsType, ref: any) => {
   const { name } = props;
+
+  useImperativeHandle(ref, () => ({
+    test() {
+      console.log('123');
+    },
+  }));
+
   return (
     <Layout>
       <Row>
@@ -18,6 +25,6 @@ const Guide: React.FC<Props> = (props) => {
       </Row>
     </Layout>
   );
-};
+});
 
 export default Guide;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,15 +2,33 @@ import Guide from '@/components/Guide';
 import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
+import { useEffect, useRef } from 'react';
 import styles from './index.less';
 
 const HomePage: React.FC = () => {
   const { name } = useModel('global');
 
+  const guideRef = useRef<any>();
+
+  useEffect(() => {
+    return () => {
+      console.log('2134');
+    };
+  }, []);
+
+  // useAsyncEffect(async () => {
+  //   await np();
+  //   // 卸载时执行
+  //   return new Promise<void>((resolve, reject) => {
+  //     console.log('卸载了');
+  //     // resolve();
+  //   })
+  // }, [])
+
   return (
     <PageContainer ghost>
       <div className={styles.container}>
-        <Guide name={trim(name)} />
+        <Guide ref={guideRef} name={trim(name)} />
       </div>
     </PageContainer>
   );
